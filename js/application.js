97515d67ecced664049e61be68c1008b10fcf665
josefina <josefina.terrera@designory.com>
2017-01-14 19:52:18 -0600

    updated clarisimo website

---
+++ js/application.js
@@ -0,0 +1,39 @@
+$(function(){
+
+	var margin_left = parseInt($('img[class*="photo"]').parent().css('marginLeft'), 10)
+	var margin_top = parseInt($('img[class*="photo"]').parent().css('marginTop'), 10)
+
+	$('img[class*="photo"]').each(function(index){
+		var position = $(this).parent().position();
+
+		$(this).css({
+			"position": "absolute",
+			"left": position.left + margin_left,
+			"top": position.top + margin_top
+		});
+	});
+
+	$('img[class*="photo"]').click(function(){
+		var is_photo = $(this).hasClass('photo');
+
+		remove_large_photo();
+
+		if(is_photo) {
+			$(this).toggleClass('photo large_photo').css({"left":200, "top": 0});
+		}	
+	});
+
+function remove_large_photo() {
+	if ($('.large_photo').length) {
+		var position = $('.large_photo').parent().position()
+		$('.large_photo').toggleClass('large_photo photo').css({
+			"left": position.left + margin_left,
+			"top": position.top + margin_top
+		});
+	}
+}
+
+
+
+});
+
